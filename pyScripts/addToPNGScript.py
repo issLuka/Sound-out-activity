ce3de1a154997d4b8ada11b7a6e53a473bb866bd
Louis <loofaunce@gmail.com>
2026-03-31 15:00:44 +0900

    made script to find the x and y coords for text boxes, jotted all them, as well as distance diffs along with some notation to follow for future ref. added fonts and edited the png script, am able to write boxes to the pngs, still working on setting up limited sizes or limiting to the box rather

--- pyScripts/addToPNGScript.py
+++ pyScripts/addToPNGScript.py
@@ -1,3 +1,16 @@
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+from PIL import Image, ImageDraw, ImageFont
+import math
+
+soundOutDesign = Image.open("../png/soundoutActivityBulbasaur.png")
+draw = ImageDraw.Draw(soundOutDesign)
+textBoxWidth, textBoxHeight = 1010, 70
+box = [(280, 497), (270 + textBoxWidth, 497 + textBoxHeight)]
+
+font = ImageFont.truetype("../comicSans.ttf", 100)
+text = "this is a test"
+fontSize = 100
+size = None
+
+
+ImageDraw.Draw(soundOutDesign).rectangle(box, outline="black")
+soundOutDesign.save("../png/soundoutActivityBulbasaurWithText.png")
