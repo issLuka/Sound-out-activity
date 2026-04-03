06dd4fbd8571af45e52817a30be0d1c74534ca18
Louis <loofaunce@gmail.com>
2026-04-03 13:02:11 +0900

    I GOT THE PNG SCRIPT WORKING (KINDA IT STILL NEEDS TO TALK TO FRONT END BUT RAHHHHHHHH

--- pyScripts/addToPNGScript.py
+++ pyScripts/addToPNGScript.py
@@ -41,12 +41,12 @@ def drawTextBox(questionsDictionary, DistanceListy1):
             for i in range(4):
                 i += 1
                 #print("level1 " + str(i))
-                draw.rectangle(levelsBoxes[i], outline="black")
+                draw.rectangle(levelsBoxes[i])
             for i in range(2):
                 i += 1
                 levelFiveBoxes = {1: [(levelsBoxes[1][0][0], levelsBoxes[1][0][1] + levelFiveToLevelOneYDistance), (levelsBoxes[1][1][0], levelsBoxes[1][1][1] + levelFiveToLevelOneYDistance)],
                                 2: [(levelsBoxes[2][0][0], levelsBoxes[2][0][1] + levelFiveToLevelOneYDistance), (levelsBoxes[2][1][0], levelsBoxes[2][1][1] + levelFiveToLevelOneYDistance)]}
-                draw.rectangle(levelFiveBoxes[i], outline="black")
+                draw.rectangle(levelFiveBoxes[i])
         elif boxCount >= 3 and boxCount <= 15: #FOR LEVELS 2 3 AND 4
             while boxCount >=4 and boxCount <= 7:
                 boxCount += 1
@@ -54,21 +54,21 @@ def drawTextBox(questionsDictionary, DistanceListy1):
                 level2YCoord = [levelsBoxes[boxNKey][0][1] + questionDistancesY1[0], levelsBoxes[boxNKey][1][1] + questionDistancesY1[0]]
                 levelsBoxes[boxNKey] = [(levelsBoxes[boxNKey][0][0], level2YCoord[0]), (levelsBoxes[boxNKey][1][0], level2YCoord[1])]
                 levelTwoBoxes = copy.copy(levelsBoxes)
-                draw.rectangle(levelsBoxes[boxNKey], outline="black")
+                draw.rectangle(levelsBoxes[boxNKey])
             while boxCount >= 8 and boxCount <= 11:
                 boxCount += 1
                 boxNKey = boxCount - 8
                 level3YCoord = [levelsBoxes[boxNKey][0][1] + questionDistancesY1[1], levelsBoxes[boxNKey][1][1] + questionDistancesY1[1]]
                 levelsBoxes[boxNKey] = [(levelsBoxes[boxNKey][0][0], level3YCoord[0]), (levelsBoxes[boxNKey][1][0], level3YCoord[1])]
                 levelThreeBoxes = copy.copy(levelsBoxes)
-                draw.rectangle(levelsBoxes[boxNKey], outline="black")
+                draw.rectangle(levelsBoxes[boxNKey])
             while boxCount >= 12 and boxCount <= 15:
                 boxCount += 1
                 boxNKey = boxCount - 12
                 level4YCoord = [levelsBoxes[boxNKey][0][1] + questionDistancesY1[2], levelsBoxes[boxNKey][1][1] + questionDistancesY1[2]]
                 levelsBoxes[boxNKey] = [(levelsBoxes[boxNKey][0][0], level4YCoord[0]), (levelsBoxes[boxNKey][1][0], level4YCoord[1])]
                 levelFourBoxes = copy.copy(levelsBoxes)
-                draw.rectangle(levelsBoxes[boxNKey], outline="black")
+                draw.rectangle(levelsBoxes[boxNKey])
     return levelTwoBoxes, levelThreeBoxes, levelFourBoxes, levelFiveBoxes
 
 drawTextBox(questionBoxDict, boxDistanceListY1)
@@ -105,13 +105,12 @@ def drawText(words):
 
         dictCount += 1
         box = levelsDict[dictCount]
-        #print("box# ", str(box))
 
         for i in range(len(wordsList["Level "+str(dictCount)+":"])): #loops thru each level
+
             key = i + 1
-            testWord = wordsList["Level "+str(key)+":"][i]
+            testWord = wordsList["Level "+str(dictCount)+":"][i]
             boxList = box[key]
-            print(key, " ", i)
             font, textWidth, textHeight = fitSingleLine(draw, testWord, boxList, "../japaneseMonospace.ttf", maxSize = 100)
 
             x = boxList[0][0]
@@ -120,8 +119,8 @@ def drawText(words):
             draw.text((x, y), testWord, font = font, anchor = "ls", fill = "black", encoding = "UTF-8")
             
 
-wordTestDict = {"Level 1:": ["ネーバリング - bihnirnggeo0000000000000ooooooo", "test", "test 1", "テスト３"], "Level 2:": ["one", "two", "three", "four"],
-                "Level 3:": ["five", "six", "seven", "eight"], "Level 4:": ["nine", "ten", "eleven", "twelve"], "Level 5:": ["thirteen", "fourteen"]}
+wordTestDict = {"Level 1:": ["ネーバリング - bihnirnggeo0000000000000ooooooo", "poop", "test 1", "テスト３"], "Level 2:": ["one", "two", "three", "four"],
+                "Level 3:": ["five", "six", "seven", "eight"], "Level 4:": ["butt", "ten", "eleven", "twelve"], "Level 5:": ["thirteen", "fourteen"]}
 drawText(wordTestDict)
 
 
